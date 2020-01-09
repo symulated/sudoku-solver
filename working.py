4ae7cf0718428f7a99ca235539bc22033944ea9f
symulated <50853161+symulated@users.noreply.github.com>
2020-01-08 21:13:03 -0400

    update working.py

--- working.py
+++ working.py
@@ -9,6 +9,33 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True 
+    else:
+        row, col = find
+
+
+def valid(bo, num, pos):
+    #check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and if pos[1] != i:
+            return False
+    #check column 
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num pos[0] != i:
+            return False
+    #check box 
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, boy_y*3+3):
+        for j in range(box_x*3,box_x*3+3):
+            if bo[i][j]==num and (i,j) != pos:
+                return False
+
+
 
 def print_board(bo):
     for i in range(len(bo)):
@@ -24,4 +51,9 @@ def print_board(bo):
             else:
                 print(str(bo[i][j])+" ", end="")
 
-print_board(board)
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j]==0;
+                return (i,j) #row, col (y,x instead of x,y)
+    return None
